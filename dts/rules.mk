58492a4dc8a133e5cd572b4f46779bde605425a8
Ivan <75212472+HiPriv228@users.noreply.github.com>
2023-01-25 10:12:14 +0500

    Deleted lenovo tab M10

--- dts/rules.mk
+++ dts/rules.mk
@@ -7,7 +7,6 @@ DTBS += \
 endif
 ifeq ($(PROJECT), msm8953-secondary)
 DTBS += \
-        $(LOCAL_DIR)/sdm450-lenovo-tb-x605f.dtb \
 	$(LOCAL_DIR)/sdm450-samsung-r04.dtb \
 	$(LOCAL_DIR)/sdm450-xiaomi-rosy.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-common.dtb \
